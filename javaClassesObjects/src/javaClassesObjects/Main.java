48b65b319861007d5773c34473746e805ff64c88
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-02-15 15:30:57 -0600

    created Pet class with 2 constructors

---
+++ javaClassesObjects/src/javaClassesObjects/Main.java
@@ -0,0 +1,10 @@
+package javaClassesObjects;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaClassesObjects/src/javaClassesObjects/Pet.java
@@ -0,0 +1,18 @@
+package javaClassesObjects;
+
+public class Pet {
+	private String name;
+	private int age;
+	private String location;
+	private String type;
+
+	public Pet() {
+	}
+
+	public Pet(String name, int age, String location, String type) {
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.type = type;
+	}
+}
